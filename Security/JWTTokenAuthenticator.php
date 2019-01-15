72a697797173331cb0d1e6ab04745c4867db90f5
Skipper <o.chyslovskyi@hubber.pro>
2019-01-15 12:58:00 +0200

    fix issue with sending http 401 response from lexic on failure

--- Security/JWTTokenAuthenticator.php
+++ Security/JWTTokenAuthenticator.php
@@ -12,6 +12,8 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator as BaseJWTTokenAuthenticator;
 use Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Garlic\Wrapper\Service\GraphQL\Exceptions\GraphQLQueryException;
 
 /**
  * Class JWTTokenAuthenticator
@@ -103,5 +105,10 @@ class JWTTokenAuthenticator extends BaseJWTTokenAuthenticator
         }
 
         return $preAuthToken;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $authException)
+    {
+        throw new GraphQLQueryException($authException->getMessage());
     }
 }
